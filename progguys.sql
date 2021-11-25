2c07e41618bbd9ffd6c986ae00b66095fb3e3f80
Hugo <maestraccihugofacebook@yahoo.fr>
2021-11-25 15:47:08 +0100

    ajout sql

---
+++ progguys.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 25 nov. 2021 à 14:46
+-- Version du serveur :  8.0.23
+-- Version de PHP : 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `progguys`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `carte`
+--
+
+DROP TABLE IF EXISTS `carte`;
+CREATE TABLE IF NOT EXISTS `carte` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_projet` int NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `titre` varchar(100) NOT NULL,
+  `l_titre` varchar(30) NOT NULL,
+  `texte` text NOT NULL,
+  `notation` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `equipe`
+--
+
+DROP TABLE IF EXISTS `equipe`;
+CREATE TABLE IF NOT EXISTS `equipe` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `img_carrousel`
+--
+
+DROP TABLE IF EXISTS `img_carrousel`;
+CREATE TABLE IF NOT EXISTS `img_carrousel` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(35) NOT NULL,
+  `img_alt` varchar(200) NOT NULL,
+  `id_projet` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projet`
+--
+
+DROP TABLE IF EXISTS `projet`;
+CREATE TABLE IF NOT EXISTS `projet` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` int NOT NULL,
+  `titre` varchar(50) NOT NULL,
+  `desc` varchar(5000) NOT NULL,
+  `carous` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `mail` varchar(99) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `equipe_id` int NOT NULL,
+  `image` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
