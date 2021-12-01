5148c2af97158f25fa8b6b75bc9a68f9e93b1e41
judegoiave <bmillan@gaming.tech>
2021-12-01 09:26:11 +0100

    sh

--- menu.php
+++ menu.php
@@ -104,7 +104,20 @@
         </div>
       </div>
     </div>
-
+    <?php require_once "config.php"; ?>
+    <html>
+     <head>
+     </head>
+     <body>
+      <?php require "menu.php"; ?>
+      <h1>Connexion</h1>
+      <form method="post" action="login.php">
+        <input type='email' name='email' />
+        <input type='password' name='password' />
+        <input type='submit' value='Me connecter' />
+      </form>
+     </body>
+    </html>
   </nav>
 </div>
 
